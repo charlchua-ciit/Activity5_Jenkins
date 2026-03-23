fb2c7593f0bf09104ca3009295034b2c797c001a
charlchua-ciit <155603936+charlchua-ciit@users.noreply.github.com>
2026-03-23 17:15:53 +0800

    Update sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -8,3 +8,6 @@ test('adds 5 + 7 to equal 12', () => {
 test('adds 6 + 6 to equal 12', () => {
   expect(sum(6, 6)).toBe(12);
 });
+test('adds 10 + 10 to equal 20', () => {
+  expect(sum(10, 10)).toBe(20);
+});
