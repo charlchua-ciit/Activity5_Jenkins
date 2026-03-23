60303f162fcc2fe0e693dd4b8491328cd1bccfe4
charlchua-ciit <155603936+charlchua-ciit@users.noreply.github.com>
2026-03-23 17:01:47 +0800

    Update sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -1,10 +1,10 @@
 const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
-expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(3);
 });
 test('adds 5 + 7 to equal 12', () => {
-expect(sum(5, 7)).toBe(12);
+  expect(sum(5, 7)).toBe(12);
 });
 test('adds 6 + 6 to equal 12', () => {
-expect(sum(6, 6)).toBe(12);
+  expect(sum(6, 6)).toBe(12);
 });
